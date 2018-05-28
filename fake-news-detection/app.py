19390b4300e8c1300259b92ec3d843959b97574d
Parth Sharma <parth261297@gmail.com>
2018-05-28 18:44:58 +0530

    Added similarity calculation to the web app

--- fake-news-detection/app.py
+++ fake-news-detection/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from logging import DEBUG
+import main
 
 
 app = Flask(__name__)
@@ -10,6 +11,17 @@ app.logger.setLevel('DEBUG')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/calculate', methods=['POST', 'GET'])
+def calculate():
+    if request.method == 'POST':
+        url = request.form['url']
+        return render_template('calculate.html', value=str(main.calculate(url)))
+    else:
+        url = request.args.get('url')
+        return render_template('calculate.html', value=str(main.calculate(url)))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
